7c9571d3abd2e38d4159c585c6082bd8e6c89a5a
xujingyi <xujingyi46@163.com>
2017-08-28 20:10:27 +0800

    修改

--- flaskr.py
+++ flaskr.py
@@ -11,6 +11,15 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(__name__)  # load config from this file , flaskr.py
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
 
 
 def connect_db():
